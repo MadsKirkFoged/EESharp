57194069a21790c1591493b1048d863e2ff6f3a4
Mads Foged <jfogedm@jci.com>
2021-05-12 14:02:26 +0200

    added a test class

---
+++ Class/testClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitsNet;
+
+namespace EESharp.Class
+{
+    public class TestClass
+    {
+
+
+        //Constants can be set this way
+        private Length LengthOftest = Length.FromCentimeters(10);
+
+
+        //Simple calculations can be easily be return like this 
+        public Area PlateConnectionCross => Area.FromCircleDiameter(LengthOftest);
+        public MassFlow MassFlowFromTest => MassFlow.FromKilogramsPerSecond(10) + MassFlow.FromKilogramsPerSecond(10);
+
+
+
+        private MassFlow massFlowFromTest1;
+        public MassFlow MassFlowFromTest1
+        {
+            get
+            {
+                return massFlowFromTest1;
+            }
+
+            set
+            {
+                massFlowFromTest1 = value;
+            }
+        }
+
+
+
+
+
+    }
+}
